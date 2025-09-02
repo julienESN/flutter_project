d389f62626400a34d27dd3e44adc71b5c2228d1b
Julien Esnault <julien.esnau@gmail.com>
2025-09-02 09:45:57 +0200

    chore(firebase): ajoute config projet (firebase.json, firebase_options.dart) et règles Firestore par utilisateur

---
+++ lib/firebase_options.dart
@@ -0,0 +1,52 @@
+import 'package:firebase_core/firebase_core.dart' show FirebaseOptions;
+import 'package:flutter/foundation.dart'
+    show defaultTargetPlatform, kIsWeb, TargetPlatform;
+
+class DefaultFirebaseOptions {
+  static FirebaseOptions get currentPlatform {
+    if (kIsWeb) {
+      return web;
+    }
+    switch (defaultTargetPlatform) {
+      case TargetPlatform.android:
+        throw UnsupportedError(
+          'DefaultFirebaseOptions have not been configured for android - '
+          'you can reconfigure this by running the FlutterFire CLI again.',
+        );
+      case TargetPlatform.iOS:
+        throw UnsupportedError(
+          'DefaultFirebaseOptions have not been configured for ios - '
+          'you can reconfigure this by running the FlutterFire CLI again.',
+        );
+      case TargetPlatform.macOS:
+        throw UnsupportedError(
+          'DefaultFirebaseOptions have not been configured for macos - '
+          'you can reconfigure this by running the FlutterFire CLI again.',
+        );
+      case TargetPlatform.windows:
+        throw UnsupportedError(
+          'DefaultFirebaseOptions have not been configured for windows - '
+          'you can reconfigure this by running the FlutterFire CLI again.',
+        );
+      case TargetPlatform.linux:
+        throw UnsupportedError(
+          'DefaultFirebaseOptions have not been configured for linux - '
+          'you can reconfigure this by running the FlutterFire CLI again.',
+        );
+      default:
+        throw UnsupportedError(
+          'DefaultFirebaseOptions are not supported for this platform.',
+        );
+    }
+  }
+
+  static const FirebaseOptions web = FirebaseOptions(
+    apiKey: 'AIzaSyBAxe0SnFl6W70Rnw8GdERVDgALVREsc0U',
+    appId: '1:1017556268592:web:c29ea9802dc59ff5251ff5',
+    messagingSenderId: '1017556268592',
+    projectId: 'flutter-7fb68',
+    authDomain: 'flutter-7fb68.firebaseapp.com',
+    storageBucket: 'flutter-7fb68.firebasestorage.app',
+    measurementId: 'G-MX4RXYMJ0D',
+  );
+}
